dfaebc471c46b5bec06cea630604911aa1b1ca2a
jrsarthak10@gmail.com <jrsarthak10@gmail.com>
2023-12-27 12:29:48 +0530

    cart : subtotal added

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -26,9 +26,18 @@ export const ShopContextProvider = (props) => {
 setCartItems((prev)=>({...prev,[itemId]:newAmount}))
     }
 
-
+const getTotalCartAmount = ()=>{
+    let totalAmount=0;
+    for(const item in cartItems){
+        if(cartItems[item]>0){
+            let itemInfo = PRODUCTS.find((product) => product.id === Number(item))
+            totalAmount+=cartItems[item] * itemInfo.price
+        }
+    }
+    return totalAmount;
+}
 const contextValue = {
-    cartItems, addToCart, removeFromCart,updateCartItemCount}
+    cartItems, addToCart, removeFromCart,updateCartItemCount,getTotalCartAmount}
 
 return (
     <ShopContext.Provider value={contextValue}>{props.children}</ShopContext.Provider>
--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -4,20 +4,32 @@ import { ShopContext } from '../../context/ShopContext'
 import { CartItems } from './CartItems'
 import './cart.css'
 
-export const Cart = () => {
-  const {cartItems} = useContext(ShopContext)
+import { useNavigate } from 'react-router-dom'
 
+export const Cart = () => {
+  const { cartItems, getTotalCartAmount } = useContext(ShopContext)
+  const totalAmount = getTotalCartAmount()
+
+  const navigate = useNavigate()
   return (
     <div className='cart'>
       <div><h1>Your Cart Items</h1>
       </div>
       <div className='cart'>
-      {PRODUCTS.map((product) => {
+        {PRODUCTS.map((product) => {
           if (cartItems[product.id] !== 0) {
             return <CartItems data={product} />;
           }
         })}
       </div>
+      {totalAmount > 0 ?
+        <div className='checkout'>
+          <h3>Subtotal: ${totalAmount}</h3>
+          <button onClick={() => navigate('/')}>Continue Shopping</button>
+          <button>Checkout</button>
+        </div> :
+        <h1>Your Cart is Empty</h1>
+      }
     </div>
   )
 }
